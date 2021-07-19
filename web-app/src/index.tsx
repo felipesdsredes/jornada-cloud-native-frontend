bae59fab2537ba5a494da6551e2793761aab744a
Vinicius Finger <vinifinger92@gmail.com>
2021-07-19 20:34:15 -0300

    Add material-ui/core and /icons

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Jornada colaborativa - Projeto Frontend</Typography>
+      </Toolbar>
+    </AppBar>
+    <Container>
+      <Typography variant="h3">This is a project in React.js</Typography>
+    </Container>
   </React.StrictMode>,
   document.getElementById("root")
 );
