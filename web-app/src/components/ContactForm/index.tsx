b41ac5983016f184d52ef0b818d04f99a03f656c
Vinicius Finger <vinifinger92@gmail.com>
2021-09-01 10:50:12 -0300

    Added api services and form submission

--- web-app/src/components/ContactForm/index.tsx
+++ web-app/src/components/ContactForm/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import { StyledFormButton, StyledTextField, StyledForm } from "./styles"
+import api from '../../services/api';
 
 export const ContactForm = () => {
+  const [name, setName] = useState(''); 
+  const [email, setEmail] = useState(''); 
+
+  async function handleSubmit() {
+    const data = { userEmail: email, userName: name};
+    const response = await api.post('/cadastro-email', data);
+    console.log(response);
+  }
+
   return (
     <StyledForm>
-      <StyledTextField required label="Nome Completo" placeholder="Vinicius Marques Finger" variant="outlined" />
-      <StyledTextField required label="Email" placeholder="vinicius.finger@email.com" type="email" variant="outlined" />
+      <StyledTextField required label="Nome Completo" placeholder="Vinicius Marques Finger" variant="outlined" onChange={e => setName(e.target.value)} />
+      <StyledTextField required label="Email" placeholder="vinicius.finger@email.com" type="email" variant="outlined" onChange={e => setEmail(e.target.value)} />
       <StyledFormButton
-        type="submit"
+        onClick={handleSubmit}
       >
         Enviar
       </StyledFormButton>
