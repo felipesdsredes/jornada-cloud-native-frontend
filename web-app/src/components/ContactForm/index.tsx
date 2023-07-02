6bb62c369fb88db4c54b66bb62bc9c6ce5add611
Guilherme Filippini <guilhermerfilippini@hotmail.com>
2023-07-02 18:42:39 -0300

    Adding feedback to the user on form submission

--- web-app/src/components/ContactForm/index.tsx
+++ web-app/src/components/ContactForm/index.tsx
@@ -1,21 +1,30 @@
-import { useState } from "react"
-import { StyledFormButton, StyledTextField, StyledForm } from "./styles"
+import { useState } from 'react';
+import { StyledFormButton, StyledTextField, StyledForm } from './styles';
 import api from '../../services/api';
 
 export const ContactForm = () => {
-  const [name, setName] = useState(''); 
-  const [email, setEmail] = useState(''); 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
   async function handleSubmit() {
-    const data = { userEmail: email, userName: name};
-    const response = await api.post('/cadastro-email', data);
-    console.log(response);
+      const data = { userEmail: email, userName: name};
+      await api.post('/cadastro-email', data).then(() => {
+        window.alert('E-mail enviado com sucesso!');
+      })
+      .catch(() => {
+        window.alert('Erro ao enviar o e-mail!');
+      })
+      .finally(() => {
+        Array.from(document.querySelectorAll("input")).forEach(
+          input => (input.value = "")
+        );
+      })
   }
 
   return (
     <StyledForm>
-      <StyledTextField required label="Nome Completo" placeholder="Vinicius Marques Finger" variant="outlined" onChange={e => setName(e.target.value)} />
-      <StyledTextField required label="Email" placeholder="vinicius.finger@email.com" type="email" variant="outlined" onChange={e => setEmail(e.target.value)} />
+      <StyledTextField required label="Nome Completo" placeholder="Your name" variant="outlined" onChange={e => setName(e.target.value)} />
+      <StyledTextField required label="Email" placeholder="youremail@email.com" type="email" variant="outlined" onChange={e => setEmail(e.target.value)} />
       <StyledFormButton
         onClick={handleSubmit}
       >
