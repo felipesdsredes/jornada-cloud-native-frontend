6226eec8e6b900d0993dd7ffd379fce6c9c8bbc7
vfinger <vinicius.finger@adp.com>
2023-07-03 20:42:25 -0300

    add MuiAlert in ContactForm

--- web-app/src/components/ContactForm/index.tsx
+++ web-app/src/components/ContactForm/index.tsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import { StyledFormButton, StyledTextField, StyledForm } from './styles';
 import api from '../../services/api';
+import { Snackbar } from "@material-ui/core";
+import MuiAlert from '@material-ui/lab/Alert';
+
+export type Color = 'success' | 'info' | 'warning' | 'error';
 
 export const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [messageAlert, setMessageAlert] = useState('');
+  const [alertSeverity, setAlertSeverity] = useState<Color>();
 
   async function handleSubmit() {
       const data = { userEmail: email, userName: name};
       await api.post('/cadastro-email', data).then(() => {
-        window.alert('E-mail enviado com sucesso!');
+        setMessageAlert('E-mail enviado com sucesso!');
+        setAlertSeverity('success');
+        setShowSnackbar(true);
       })
       .catch(() => {
-        window.alert('Erro ao enviar o e-mail!');
+        setMessageAlert('Erro ao enviar o e-mail!');
+        setAlertSeverity('error');
+        setShowSnackbar(true);
       })
       .finally(() => {
         Array.from(document.querySelectorAll("input")).forEach(
@@ -30,6 +41,9 @@ export const ContactForm = () => {
       >
         Enviar
       </StyledFormButton>
+      <Snackbar open={showSnackbar} autoHideDuration={6000} onClose={() => setShowSnackbar(false)} anchorOrigin={{horizontal: 'center', vertical: 'bottom'}}>
+        <MuiAlert elevation={6} variant="filled" severity={alertSeverity}>{messageAlert}</MuiAlert>
+      </Snackbar>
     </StyledForm>
   )
 }
